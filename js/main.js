a41580172fc48f4ec8676b532f08a6835632d3e9
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-03-30 14:43:01 +0300

    adapt the header, add a burger-menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ const swiper = new Swiper('.reviews__slider', {
     prevEl: '.reviews__item-prev',
   },
 });
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menuList.classList.toggle('menu__list--open');
+});
