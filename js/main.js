50994a37b6ef05274812558c29fd19e3890a4f1f
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-04-03 21:21:25 +0300

    add js for accordion in the questions section

--- js/main.js
+++ js/main.js
@@ -15,3 +15,22 @@ const menuList = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', () => {
   menuList.classList.toggle('menu__list--open');
 });
+
+const accordeonTitle = document.querySelectorAll('.accordeon__title');
+
+accordeonTitle.forEach(item => {
+  item.addEventListener('click', () => {
+    const parent = item.parentNode;
+
+    if (parent.classList.contains('accordeon__item--active')) {
+      parent.classList.remove('accordeon__item--active');
+    } else {
+      accordeonTitle.forEach(element => {
+        element.parentNode.classList.remove('accordeon__item--active');
+      });
+
+      parent.classList.add('accordeon__item--active');
+    }
+    // item.parentNode.classList.toggle('accordeon__item--active');
+  });
+});
